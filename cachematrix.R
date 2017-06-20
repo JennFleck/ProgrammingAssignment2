ae3e8f24457e8fb945bcb0b3b69af930035e9ba9
Jennifer Fleck <jennifer.fleck@milliman.com>
2017-06-20 14:48:51 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take advantage of lexical scoping to cache the inversion of a 
+## matrix to save run time if the same inversion is later needed.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a set of functions: set, get, setinversion, getinversion 
+## in a list to allow for cacheing of a matrix inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mi <- NULL
+        set <- function(y){
+                x <<- y
+                mi <<- NULL
+        }
+        get <- function() x
+        setinversion <- function(z) mi <<- z
+        getinversion <- function() mi
+        list(set = set, get = get,
+             setinversion = setinversion, 
+             getinversion = getinversion)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns a matrix that is the inverse of x.  Pulls from the cached
+## inversion if it has already been calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        mi <- x$getinversion()
+        if(!is.null(mi)) {
+                message("getting cached data")
+                return(mi)
+        }
+        dat <- x$get()
+        mi <- solve(dat, ...)
+        x$setinversion(mi)
+        return(mi)
 }
